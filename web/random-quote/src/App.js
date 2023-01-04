42795ec82cf636dd2ed526ed94e0bf569783ca43
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-01-04 22:33:29 +0100

    .

--- web/random-quote/src/App.js
+++ web/random-quote/src/App.js
@@ -1,34 +1,4 @@
 import './App.css';
-import raw from "./rawtext.txt";
-
-let data = "";
-
-function getData() {
-  return fetch(raw)
-  .then(Response => Response.text())
-  .then(text => data = text)
-}
-
-data = getData();
-console.log(data);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
 
 function App() {
   return (
---
+++ web/random-quote/src/python/getQuotes.py
@@ -0,0 +1,21 @@
+import os
+from urllib.request import urlopen
+import re
+
+text = urlopen("http://www.quotationspage.com/random.php")
+data = str(text.read())
+
+os.chdir("./src/python/")
+
+
+list = re.findall(r'(class="quote">)(.+)(.</a>)', data, re.DOTALL)
+
+#list2 = re.findall(r'', list, re.DOTALL)
+
+
+f = open("rawtext.txt", "w")
+for i in list:
+    f.writelines(str(i))
+f.close()
+
+
