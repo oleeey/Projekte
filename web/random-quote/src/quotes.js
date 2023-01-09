2a8667edf710711bec8f99c9f5becef74a9ac4ed
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-01-09 14:03:30 +0100

    Added quotes

--- web/random-quote/src/quotes.js
+++ web/random-quote/src/quotes.js
@@ -8,6 +8,26 @@ export let quotes = [
     ["Well, spring sprang. We've had our state of grace and our little gift of sanctioned madness, courtesy of Mother Nature. Thanks, Gaia. Much obliged. I guess it's time to get back to that daily routine of living we like to call normal.","David Assael"],
     ["One man's folly is another man's wife.","Helen Rowland"],
     ["Toughness doesn't have to come in a pinstriped suit.","Senator Dianne Feinstein"],
-    ["Equal opportunity means everyone will have a fair chance at being incompetent.","Laurence J. Peter"]
+    ["Equal opportunity means everyone will have a fair chance at being incompetent.","Laurence J. Peter"],
+    ["England and America are two countries separated by a common language.","George Bernard Shaw"],
+    ["Those that think it permissible to tell white lies soon grow color blind.","Austin O'Malley"],
+    ["A cynic is not merely one who reads bitter lessons from the past, he is one who is prematurely disappointed in the future.","Sidney J. Harris"],
+    ["Illegitimacy is something we should talk about in terms of not having it.","Dan Quayle"],
+    ["There are no such things as applied sciences, only applications of science.","Louis Pasteur"],
+    ["Ask about your neighbors, then buy the house.","Jewish Proverb"],
+    ["Say little, and love much; give all; judge no man; aspire to all that is pure and good.","White Eagle"],
+    ["The reason there are so few female politicians is that it is too much trouble to put makeup on two faces.","Maureen Murphy"],
+    ["Trumpet in a herd of elephants; crow in the company of cocks; bleat in a flock of goats.","Malayan Proverb"],
+    ["Politics is perhaps the only profession for which no preparation is thought necessary.","Robert Louis Stevenson"],
+    ["An effective way to deal with predators is to taste terrible.","Unknown"],
+    ["He has all the virtues I dislike and none of the vices I admire.","Sir Winston Churchill"],
+    ["His mother should have thrown him away and kept the stork.","Mae West"],
+    ["I like persons better than principles, and I like persons with no principles better than anything else in the world.","Oscar Wilde"],
+    ["I wish you sunshine on your path and storms to season your journey. I wish you peace in the world in which you live... More I cannot wish you except perhaps love to make all the rest worthwhile.","Robert A. Ward"],
+    ["Be brief, for no discourse can please when too long.","Miguel de Cervantes"],
+    ["The Army has carried the American ... ideal to its logical conclusion. Not only do they prohibit discrimination on the grounds of race, creed and color, but also on ability.","Tom Lehrer"],
+    ["There are more fools in the world than there are people.","Heinrich Heine"],
+    ["Happiness: a good bank account, a good cook and a good digestion.","Jean Jacques Rousseau"],
+    ["It is never too late to give up our prejudices.","Henry David Thoreau"]
 ];
 
