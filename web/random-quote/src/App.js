2911b960b419b0725ccd58620f1cb623bc15fac2
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-01-09 14:03:43 +0100

    Added color change

--- web/random-quote/src/App.js
+++ web/random-quote/src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import {quotes} from './quotes.js';
 import React from 'react';
 
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       quotesList: quotes,
       quote: "",
-      author: ""
+      author: "",
+      color: ""
     }
     this.getQuote = this.getQuote.bind(this);
+    this.getRandomColor = this.getRandomColor.bind(this);
   }
 
   getQuote() {
@@ -28,17 +28,33 @@ class App extends React.Component {
     }
   }
 
+  getRandomColor() {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    console.log(randomColor);
+    this.setState({
+      color: "#" + randomColor
+    });
+  }
+
   componentDidMount() {
     this.getQuote();
+    this.getRandomColor();
   }
 
 
+  
   render() {
+    document.body.style.backgroundColor = this.state.color;
     return (
       <div className="App" id='quote-box'>
         <p id='text'>{this.state.quote}</p>
         <p id="author">{this.state.author}</p>
-        <button id='new-quote' onClick={this.getQuote}>New quote</button>
+        <button id='new-quote' 
+        onClick={() => {this.getQuote(); this.getRandomColor();}}
+        style={{backgroundColor: this.state.color}}
+        
+        >New quote
+        </button>
       </div>
     );
   }
