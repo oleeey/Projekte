6680d1b126514b43e69d178edc841a71ccdcd879
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-01-19 21:49:03 +0100

    import all data

--- eu4-unit-compare/server/server.js
+++ eu4-unit-compare/server/server.js
@@ -8,12 +8,16 @@ const fs = require('fs')
 const csv = require('fast-csv');
 const data = []
 
-function getData(name) {
-    fs.createReadStream(`./data/${name}.csv`)
+
+let inf = [fs.createReadStream('./data/inf.csv'), '/inf'];
+let cav = [fs.createReadStream('./data/cav.csv'), '/cav'];
+
+function getData(data) {
+    data[0]
     .pipe(csv.parse({}))
     .on('error', error => console.error(error))
     .on('data', row => data.push(row))
-    .on('end', () => app.get(`/${name}`, function (req, res) {
+    .on('end', () => app.get(data[1], function (req, res) {
                         res.json({
                             data
                         })
@@ -24,9 +28,8 @@ function getData(name) {
                     });
 }
 
-let units = ["inf", "cav", "art"];
-getData("inf")
-
+getData(inf);
+getData(cav);
 
 var server = app.listen(5000, function () {
     console.log('Server is running..');
