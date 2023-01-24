5a64b1d8e33aca4f03460984d5fcf20b2af81999
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-01-24 15:35:43 +0100

    .

--- markdown-preview/script.js
+++ markdown-preview/script.js
@@ -1,10 +1,39 @@
  $(document).ready(function() {
+    setInput();
 
-    $("#editor").on("change keyup keydown paste", function() {
-        $(this).html($(this).val())
+    $("#editor").on("change keyup paste", function() {
+        setNewInput();
     });
 
  })
+
+ function getInput() {
+    return $("#editor").val().split("\n");
+ }
+
+ function setInput() {
+    let input = getInput();
+    console.log(input);
+    for (let i = 1; i <= input.length; i++) {
+        let line = input[i - 1];
+        let regex = /^#/g;
+        if (line.match(regex)) {
+            console.log(line)
+        }
+        else {
+            $("<p>", {id: "p" + i}).text(line).appendTo($("#preview"))
+        }
+    }
+ }
+
+ function setNewInput() {
+    $("#preview").empty();
+    setInput();
+ }
+
+
+
+
 
 
 
