72773aecb7c0a6f07dc4697bb11716821911f834
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-02-24 20:48:59 +0100

    .

--- drum-machine/script.js
+++ drum-machine/script.js
@@ -1,38 +1,38 @@
 $(document).ready(function() {
     $('#padItem1').on("click", function() {
-      playSound("./sounds/kick.mp3");
+      playSound("./sounds/kick.mp3", "Kick");
       });
 
     $('#padItem2').on("click", function() {
-      playSound("./sounds/clap.mp3", 0.3);
+      playSound("./sounds/clap.mp3", "Clap", 0.3);
     });
 
     $('#padItem3').on("click", function() {
-      playSound("./sounds/snare.mp3");
+      playSound("./sounds/snare.mp3", "Snare");
     });
 
     $('#padItem4').on("click", function() {
-      playSound("./sounds/808.mp3");
+      playSound("./sounds/808.mp3", "808");
     });
 
     $('#padItem5').on("click", function() {
-      playSound("./sounds/donk.mp3");
+      playSound("./sounds/donk.mp3", "Hardbass Donk");
     });
 
     $('#padItem6').on("click", function() {
-      playSound("./sounds/hihat.mp3");
+      playSound("./sounds/hihat.mp3", "Closed Hi-Hat");
     });
 
     $('#padItem7').on("click", function() {
-      playSound("./sounds/oof.mp3", 0.3);
+      playSound("./sounds/oof.mp3", "Minecraft Oof", 0.3);
     });
 
     $('#padItem8').on("click", function() {
-      playSound("./sounds/oof2.mp3");
+      playSound("./sounds/oof2.mp3", "Roblox Oof");
     });
 
     $('#padItem9').on("click", function() {
-      playSound("./sounds/cheeki.mp3");
+      playSound("./sounds/cheeki.mp3", "Cheeki Breeki", 0.3);
     });
     
 
@@ -41,31 +41,31 @@ $(document).ready(function() {
           let key = e.which;
           switch (key) {
             case 113:
-              buttonAnimation("#padItem1");
+              buttonAnimation("#padItem1", "Kick");
               break;
             case 119:
-              buttonAnimation("#padItem2");
+              buttonAnimation("#padItem2", "Clap");
               break;
             case 101:
-              buttonAnimation("#padItem3");
+              buttonAnimation("#padItem3", "Snare");
               break;
             case 97:
-              buttonAnimation("#padItem4");
+              buttonAnimation("#padItem4", "808");
               break;
             case 115:
-              buttonAnimation("#padItem5");
+              buttonAnimation("#padItem5", "Hardbass Donk");
               break;
             case 100:
-              buttonAnimation("#padItem6");
+              buttonAnimation("#padItem6", "Closed Hi-Hat");
               break;
             case 121:
-              buttonAnimation("#padItem7");
+              buttonAnimation("#padItem7", "Minecraft Oof");
               break;
             case 120:
-              buttonAnimation("#padItem8");
+              buttonAnimation("#padItem8", "Roblox Oof");
               break;
             case 99:
-              buttonAnimation("#padItem9");
+              buttonAnimation("#padItem9", "Cheeki Breeki");
               break;
           }  
       });
@@ -73,18 +73,19 @@ $(document).ready(function() {
 
  })
 
- function playSound(sound, volume = 0.5) {
+ function playSound(sound, name, volume = 0.5) {
     let audio = new Audio(sound);
     audio.volume = volume;
     audio.play();
+    $("#displaySound").text(name)
  }
 
- function buttonAnimation(item) {
-  $(item).click().toggleClass("active");
+ function buttonAnimation(element, name) {
+  $(element).click().toggleClass("active");
   setTimeout(
     function()
     {
-      $(item).toggleClass("active");
+      $(element).toggleClass("active");
     }, 100);
  }
 
